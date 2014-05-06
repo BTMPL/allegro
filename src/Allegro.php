a4e62c38e5d551b845f2ba42e5e79ffd3152ae86
Bartosz Szczecinski <btm@anfo.pl>
2014-05-06 07:19:12 +0200

    Init

---
+++ src/Allegro.php
@@ -0,0 +1,56 @@
+<?
+
+namespace Allegro;
+
+class Allegro {
+
+    private $_client = NULL;
+    private $_session = NULL;
+    
+    private $versionKeys = array();
+	
+	private $key;
+	private $countryId;
+
+    function __construct($key, $countryId = 1) {
+        
+        $options = array();
+        $options['features'] = SOAP_SINGLE_ELEMENT_ARRAYS;
+        $this->_client = new \SoapClient('https://webapi.allegro.pl/service.php?wsdl', $options);
+        $request = array(
+            'countryId' => $countryId,
+            'webapiKey' => $key
+        );
+		
+		$this->key = $key;
+		$this->countryId = $countryId;
+        
+        $sys = $this->_client->doQueryAllSysStatus($request);
+        foreach ($sys->sysCountryStatus->item as $row) {
+            $this->versionKeys[$row->countryId] = $row;
+        }
+    }
+
+    function login($login, $password) {
+        $request = array(
+            'userLogin' => $login,
+            'userPassword' => $password,
+            'countryCode' => $this->countryId,
+            'webapiKey' => $this->key,
+            'localVersion' => $this->versionKeys[$this->countryId]->verKey,
+        );
+        $this->_session = $this->_client->doLogin($request);
+    }
+
+    function __call($name, $arguments) {
+        $arguments = (array) $arguments[0];
+        $arguments['sessionId'] = $this->_session->sessionHandlePart;
+        $arguments['sessionHandle'] = $this->_session->sessionHandlePart;
+        $arguments['webapiKey'] = $this->key;
+        $arguments['countryId'] = $this->countryId;
+        $arguments['countryCode'] = $this->countryId;
+
+        return $this->_client->$name($arguments);
+    }
+    
+}
---
+++ src/PaymentType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Allegro;
+
+class PaymentType {
+    const PAYU = 1;
+    const PRZELEW = 2;
+    const ODBIOR = 4;
+}
