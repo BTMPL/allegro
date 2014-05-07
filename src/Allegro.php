06f8ba510a22fa9b25e638dccdff4870ced86f51
Bartosz Szczecinski <btm@anfo.pl>
2014-05-07 15:14:10 +0200

    code cleanup (PSR), some variable visibility changes

--- src/Allegro.php
+++ src/Allegro.php
@@ -1,38 +1,39 @@
 <?
-
 namespace Allegro;
 
-class Allegro {
+class Allegro 
+{
+    protected $client = NULL;
+    protected $session = NULL;
 
-    private $_client = NULL;
-    private $_session = NULL;
+    protected $versionKeys = array();
 
-    private $versionKeys = array();
+    protected $key;
+    protected $countryId;
 
-    private $key;
-    private $countryId;
-
-    function __construct($key, $countryId = 1, $wsdl = 'https://webapi.allegro.pl/service.php?wsdl') {
-
+    function __construct($key, $countryId = 1, $wsdl = 'https://webapi.allegro.pl/service.php?wsdl') 
+    {
+        $this->key = $key;
+        $this->countryId = $countryId;
+        
         $options = array();
         $options['features'] = SOAP_SINGLE_ELEMENT_ARRAYS;
         $options['trace'] = true;
-        $this->_client = new \SoapClient($wsdl, $options);
+        $this->client = new \SoapClient($wsdl, $options);
         $request = array(
             'countryId' => $countryId,
             'webapiKey' => $key
         );
 
-        $this->key = $key;
-        $this->countryId = $countryId;
 
-        $sys = $this->_client->doQueryAllSysStatus($request);
-        foreach ($sys->sysCountryStatus->item as $row) {
+        $status = $this->client->doQueryAllSysStatus($request);
+        foreach ($status->sysCountryStatus->item as $row) {
             $this->versionKeys[$row->countryId] = $row;
         }
     }
 
-    function login($login, $password) {
+    function login($login, $password) 
+    {
         $request = array(
             'userLogin' => $login,
             'userPassword' => $password,
@@ -40,20 +41,21 @@ class Allegro {
             'webapiKey' => $this->key,
             'localVersion' => $this->versionKeys[$this->countryId]->verKey,
         );
-        $this->_session = $this->_client->doLogin($request);
+        $this->session = $this->client->doLogin($request);
     }
 
-    function __call($name, $arguments) {
-        if(isset($arguments[0])) $arguments = (array) $arguments[0];
+    function __call($name, $arguments) 
+    {
+        if(isset($arguments[0])) $arguments = (array)$arguments[0];
         else $arguments = array();
 
-        $arguments['sessionId'] = $this->_session->sessionHandlePart;
-        $arguments['sessionHandle'] = $this->_session->sessionHandlePart;
+        $arguments['sessionId'] = $this->session->sessionHandlePart;
+        $arguments['sessionHandle'] = $this->session->sessionHandlePart;
         $arguments['webapiKey'] = $this->key;
         $arguments['countryId'] = $this->countryId;
         $arguments['countryCode'] = $this->countryId;
 
-        return $this->_client->$name($arguments);
+        return $this->client->$name($arguments);
     }
 
 }
